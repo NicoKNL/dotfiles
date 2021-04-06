f2497e4c743ab5a730b6cfee4cf21f175eb1419f
NicoKNL <klaassen.nico@gmail.com>
2021-04-06 22:55:47 +0200

    onedark

--- nvim/lua/style.lua
+++ nvim/lua/style.lua
@@ -2,4 +2,5 @@ local execute = vim.api.nvim_command
 
 execute("autocmd VimEnter * set termguicolors")
 execute("autocmd VimEnter * colorscheme onedark")
+execute("autocmd VimEnter * hi Normal guibg=NONE ctermbg=NONE")
 execute("let g:airline_theme='onedark'")
