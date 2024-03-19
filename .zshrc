8fa6919eb1449d4d656a912ff96f6c0f448bfb1c
Nico Klaassen <klaassen.nico@gmail.com>
2024-03-19 21:05:37 +0100

    revamp

--- .zshrc
+++ .zshrc
@@ -1,18 +1,3 @@
 export PATH=/opt/homebrew/bin:$PATH
 export PATH=/Applications/nvim/bin:$PATH
-
-alias solmake="clang++ --std=c++17 -o sol sol.cpp"
-
-export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="powerlevel10k/powerlevel10k"
-
-plugins=(git)
-source $ZSH/oh-my-zsh.sh
-
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
-
-if [ -z "$TMUX" ]
-then
-    tmux attach -t TMUX || tmux new -s TMUX
-fi
+export PATH=/Users/nico/Library/Python/3.9/bin:$PATH
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,73 +1,20 @@
-require('options')
-require('plugins')
-require('theme')
-
-if not vim.g.vscode then
-    require('nvim-tree').setup()
-    local nvim_tree_events = require('nvim-tree.events')
-    local bufferline_api = require('bufferline.api')
-
-    local function get_tree_size()
-      return require'nvim-tree.view'.View.width
-    end
-
-    nvim_tree_events.subscribe('TreeOpen', function()
-      bufferline_api.set_offset(get_tree_size())
-    end)
-
-    nvim_tree_events.subscribe('Resize', function()
-      bufferline_api.set_offset(get_tree_size())
-    end)
-
-    nvim_tree_events.subscribe('TreeClose', function()
-      bufferline_api.set_offset(0)
-    end)
-
-    require('lualine').setup()
-
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
 
-local utils = require('utils.selection')
-
-leaders = {'\\', ' '}
-
-for i, leader in ipairs(leaders) do
-    vim.g.mapleader = leader
-
-    if not vim.g.vscode then
-        vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeFocus<cr>') -- Open file tree
-
-        -- Telescope requires floating window, which does not work in VSCode
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- Search all files
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})  -- Perform a live grep
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})    -- Search through all open buffers
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})  -- Search through the help documentation
-    end
-
-    -- NAVIGATION
-    vim.keymap.set('n', '<leader>h', '<C-w>h') -- Move to window LEFT
-    vim.keymap.set('n', '<leader>j', '<C-w>j') -- Move to window DOWN
-    vim.keymap.set('n', '<leader>k', '<C-w>k') -- Move to window UP
-    vim.keymap.set('n', '<leader>l', '<C-w>l') -- Move to window RIGHT
-
-    vim.keymap.set('v', '<leader>rev', utils.reverseLines) -- Reverse order of selected lines
-
-    vim.keymap.set('v', '<leader>up', 'gU') -- Make selection uppercase
-    vim.keymap.set('v', '<leader>lo', 'gu') -- Make selection lowercase
-end
-
--- Auto close nvimtree when last buffer is closed
--- TODO: make it play nice with barbar. If multiple buffers are open as tabs,
---       then this function should NOT close nvim.
-vim.api.nvim_create_autocmd("BufEnter", {
-  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
-  pattern = "NvimTree_*",
-  callback = function()
-    local layout = vim.api.nvim_call_function("winlayout", {})
-    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
-  end
-})
-
-
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
