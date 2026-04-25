ac081e87fb5a62c38c09a210894843f27feca7e5
Nico Klaassen <klaassen.nico@gmail.com>
2026-04-25 16:51:29 +0200

    fix: migrate treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,13 @@ vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter",
 })
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "python" },
-  highlight = { enable = true },
-  indent = { enable = true },
+-- Install Python parser
+require("nvim-treesitter").install({ "python" })
+
+-- Highlighting via Neovim core (0.12+)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "python" },
+  callback = function()
+    vim.treesitter.start()
+  end,
 })
