c88ceb5a5bed7c0836b7e4a450186594d3fbe363
Nico Klaassen <klaassen.nico@gmail.com>
2026-04-25 16:44:47 +0200

    remove guard

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,7 @@ vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter",
 })
 
-local ok, configs = pcall(require, "nvim-treesitter.configs")
-if not ok then
-  vim.notify("nvim-treesitter not ready. Restart nvim after install.", vim.log.levels.WARN)
-  return
-end
-
-configs.setup({
+require("nvim-treesitter.configs").setup({
   ensure_installed = { "python" },
   highlight = { enable = true },
   indent = { enable = true },
