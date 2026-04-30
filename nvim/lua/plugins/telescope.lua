69ba7418baa7ea6d1dcc1f10089af181a7fddb5b
Nico Klaassen <klaassen.nico@gmail.com>
2026-04-30 22:09:35 +0200

    telescope for branch interaction

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@
 -- Keymaps:
 --   <leader>ff  find files (including hidden/dotfiles)
 --   <leader>fg  live grep (requires ripgrep)
---   <leader>fb  open buffers
+--   <leader>fb  git branches (checkout/create)
 --   <leader>fs  LSP document symbols
 --   <leader>fr  recently opened files
 
@@ -43,7 +43,7 @@ local map = vim.keymap.set
 
 map("n", "<leader>ff", function() builtin.find_files({ hidden = true }) end, { desc = "Find files" })
 map("n", "<leader>fg", builtin.live_grep,            { desc = "Live grep" })
-map("n", "<leader>fb", builtin.buffers,              { desc = "Find buffers" })
+map("n", "<leader>fb", builtin.git_branches,          { desc = "Find branches" })
 map("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document symbols" })
 map("n", "<leader>fr", builtin.oldfiles,             { desc = "Recent files" })
 map("n", "<leader>fh", builtin.help_tags,            { desc = "Help tags" })
